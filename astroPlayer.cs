2c067bbd5dba6f52580b34068226f122de4525e2
Alejandro <ahervella@me.com>
2020-08-08 17:27:37 -0500

    lot of progress
    -went back into godot to get proper ratios and PPU scaling
    -was then able to translate pixel movement numbers very nicely
    -need to clean up slide on slope and wall shtuff

--- astroPlayer.cs
+++ astroPlayer.cs
@@ -11,19 +11,21 @@ public class astroPlayer : MonoBehaviour
 {
     const float MIN_MOVE_DIST = 0.001f;
     //if this is min dist too small, game can break
-    const float GROUNDED_MIN_DIST = 0.5f;
+    const float GROUNDED_MIN_DIST = 5f;
     const float GROUNDED_MIN_COLL_DIST = GROUNDED_MIN_DIST / 10;
     const float MIN_GROUND_DEG_ANG = 40f;
     float MIN_GROUND_NORM = Mathf.Sin(Mathf.Deg2Rad * MIN_GROUND_DEG_ANG);
-    const float SLIDE_FRIC_THRESHOLD = 0.008f;
+    const float SLIDE_FRIC_THRESHOLD = 1f;
 
-    const float SPEED = 10f;
-    const float MAX_SPEED = 10f;
-    const float GRAVITY = 3f;
-    const float TERMINAL_VEL = 5f;
+    const float ACCEL = 6f/2f;
+    const float AIR_ACCEL = ACCEL / 4f;
+    const float MAX_SPEED = 160f / 2f;
+    const float MAX_AIR_SPEED = MAX_SPEED;
+    const float GRAVITY = 180f / 2f;
+    const float TERMINAL_VEL = 200f / 2f;
 
-    const float JUMP_VERT_SPEED = 0.3f;
-    const float MAX_JUMP_TIME = 0.25f;
+    const float JUMP_VERT_SPEED = 150f/2f;
+    const float MAX_JUMP_TIME = 0.6f;
 
     Rigidbody2D rb;
     Animator anim;
@@ -41,6 +43,7 @@ public class astroPlayer : MonoBehaviour
 
     public Vector2 groundNorm = new Vector2(0, 0);
     public bool grounded = false;
+    public bool onWall = false;
 
     //just for debugging
     public float hbd = 0f;
@@ -116,9 +119,18 @@ public class astroPlayer : MonoBehaviour
 
         if (grounded) { jumpTimeCounter = 0f; }
 
+
+        //get new x and y velocity\
+        float xVel = vel.x;
+
+        float accel = grounded ? ACCEL : AIR_ACCEL;
+        accel = direction.x == 0 ? -accel : accel;
+        float dirSign = direction.x * vel.x;
+        if (dirSign <= 0 || (dirSign > 0 && MAX_SPEED > vel.x))
+        {
+            xVel = Mathf.Lerp(vel.x, direction.x * MAX_SPEED, Time.deltaTime * ACCEL);//ACCEL);//vel.x + accel * Time.fixedDeltaTime;
+        }
         
-        //get new x and y velocity
-        float xVel = Mathf.Lerp(vel.x, direction.x * SPEED, Time.fixedDeltaTime);
 
         //makes sense to multiply by deltaTime here and later
         //because grav is acceleration
@@ -127,7 +139,9 @@ public class astroPlayer : MonoBehaviour
         
 
         //clamp limits
-        xVel = Mathf.Clamp(xVel, -MAX_SPEED, MAX_SPEED);
+
+        float maxX = Mathf.Max(Mathf.Abs(vel.x), MAX_SPEED);
+        xVel = Mathf.Clamp(xVel, -maxX, maxX);
         yVel = Mathf.Clamp(yVel, -TERMINAL_VEL, TERMINAL_VEL);
 
         if (jumping && canJump)
@@ -135,7 +149,8 @@ public class astroPlayer : MonoBehaviour
             if (jumpTimeCounter <= MAX_JUMP_TIME)
             {
                 jumpTimeCounter += Time.fixedDeltaTime;
-                yVel += JUMP_VERT_SPEED;
+                yVel = JUMP_VERT_SPEED;// * Time.fixedDeltaTime * 4;
+
             }
 
             
@@ -165,7 +180,7 @@ public class astroPlayer : MonoBehaviour
     {
         gravityDir.Normalize();
 
-        //the amount to move during this frame
+        //the amount to move during this frame 
         Vector2 frameDeltaPos = velocity * frameTime;
 
         //where collision data will be stored after rb.Cast
@@ -184,36 +199,34 @@ public class astroPlayer : MonoBehaviour
 
 
 
-        //Logic for sticking to the surface and not flying off slope
-        //also for not sliding on slope
-        Vector2 snapVect = Vector2.zero;
-        Vector2 fricVect = Vector2.zero;
+
+
+        bool prevGroundState = grounded;
 
         grounded = false;
+        onWall = false;
         bool useGroundForColl = false;
+        RaycastHit2D closestGroundColl = new RaycastHit2D();
+        float groundDegIncline = 0f;
+
 
         if (groundCheck > 0)
         {
-            RaycastHit2D closestGroundColl = hitBufferGrav[0];
+            closestGroundColl = hitBufferGrav[0];
             for (int i = 1; i < collCount; i++)
             {
                 closestGroundColl = closestGroundColl.distance < hitBufferGrav[i].distance ? closestGroundColl : hitBufferGrav[i];
             }
 
-            grounded = closestGroundColl.distance <= GROUNDED_MIN_DIST;
+            groundDegIncline = 180 - Vector2.Angle(closestGroundColl.normal, gravityDir);
+
+
+            grounded = closestGroundColl.distance <= GROUNDED_MIN_DIST && groundDegIncline <= MIN_GROUND_DEG_ANG;
+            onWall = closestGroundColl.distance <= GROUNDED_MIN_DIST && groundDegIncline > MIN_GROUND_DEG_ANG;
             useGroundForColl = closestGroundColl.distance <= GROUNDED_MIN_COLL_DIST;
 
             //will always return acuter angle
-            float groundDegIncline = 180 - Vector2.Angle(closestGroundColl.normal, gravityDir);
 
-            if (groundDegIncline <= MIN_GROUND_DEG_ANG)
-            {
-                //TODO: what about distance from horiz. movement? Need to cast after horiz movement.
-                if (snap) { snapVect = gravityDir * closestGroundColl.distance; }
-
-                //if (useGroundForColl && stopOnSlope && frameDeltaPos.magnitude < slideThreshold) { fricVect = frameDeltaPos * -1; }
-
-            }
 
             //else { frameDeltaPos -= gravityDir * Vector2.Dot(frameDeltaPos, gravityDir) * 1.5f; }
 
@@ -223,7 +236,7 @@ public class astroPlayer : MonoBehaviour
 
 
 
-        bool prevGroundState = grounded;
+        
         //grounded = groundCheck > 0;
 
 
@@ -239,12 +252,13 @@ public class astroPlayer : MonoBehaviour
             hitBuffer = hitBufferGrav;
         }
 
+        RaycastHit2D closestColl = new RaycastHit2D();
 
         //if there were collisions with surface
         if (collCount > 0)
         {
             //get the closest collision
-            RaycastHit2D closestColl = hitBuffer[0];
+            closestColl = hitBuffer[0];
             for (int i = 1; i < collCount; i++)
             {
                 closestColl = closestColl.distance < hitBuffer[i].distance ? closestColl : hitBuffer[i];
@@ -272,16 +286,49 @@ public class astroPlayer : MonoBehaviour
 
         }
 
-        Vector2 innerFaceNorm = frameDeltaPos.x < 0? Vector2.Perpendicular(frameDeltaPos) : Vector2.Perpendicular(frameDeltaPos) * -1;
-        slideVect = innerFaceNorm;//
-        slideVectorLength = (innerFaceNorm).magnitude;
-        
+        //Vector2 innerFaceNorm = frameDeltaPos.x < 0? Vector2.Perpendicular(frameDeltaPos) : Vector2.Perpendicular(frameDeltaPos) * -1;
+        //slideVect = innerFaceNorm;//Sie
 
-        
+        //slideVectorLength = (innerFaceNorm).magnitude;
+
+
+        //Logic for sticking to the surface and not flying off slope
+        //also for not sliding on slope
+        Vector2 snapVect = Vector2.zero;
+        Vector2 fricVect = Vector2.zero;
+        Vector2 slideVect = Vector2.zero;
+
+        if (groundCheck > 0)
+        {
+
+            if (groundDegIncline <= MIN_GROUND_DEG_ANG)
+            {
+                //TODO: what about distance from horiz. movement? Need to cast after horiz movement.
+                if (snap) { snapVect = gravityDir * closestGroundColl.distance; }
+
+                if (useGroundForColl && stopOnSlope && frameDeltaPos.magnitude < slideThreshold) { fricVect = frameDeltaPos * -1; }
+
+            }
+            else if (useGroundForColl)
+            {
+                //Vector2 parallel2Floor = Vector2.Perpendicular(closestColl.normal);
+                //parallel2Floor = Vector2.Angle(parallel2Floor, gravityDir) < 90? parallel2Floor : parallel2Floor * -1;
+
+                //slideVect = gravityDir * frameDeltaPos.magnitude;
+            }
+        }
+
+         snapVect = Vector2.zero;
+         fricVect = Vector2.zero;
+         slideVect = Vector2.zero;
+
 
 
         //move the character to the new position
-        rb.MovePosition(rb.position + frameDeltaPos + snapVect + fricVect);
+        Debug.Log("fdp: " + frameDeltaPos.ToString("F10"));
+        Debug.Log("snapV: " + snapVect.ToString("F10"));
+        Debug.Log("fricVect: " + fricVect.ToString("F10"));
+        rb.MovePosition(rb.position + frameDeltaPos + snapVect + fricVect + slideVect);
 
         Debug.DrawLine(bottomOfShape, bottomOfShape + frameDeltaPos / Time.deltaTime, Color.green);
 
