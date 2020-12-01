76ba7fded4c2d5517c6d361a7a2c285b9527352b
Alejandro Hervella <>
2020-11-30 22:39:08 -0500

    - update git ignore and location some left over AstroAnim stuff from a while ago

--- AstroAnim.cs
+++ AstroAnim.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class AstroAnim : MonoBehaviour
 {
-    public enum ANIM { END1, END2, FALL, JUMP, LAND, RUN, STAND, START, DEATH }
+    public enum ANIM { END1, END2, FALL, JUMP, LAND, RUN, STAND, START, DEATH, NONE }
     const int GAME_FPS = 60;
     Animator anim;
     AnimatorOverrideController animOC;
@@ -47,6 +47,80 @@ public class AstroAnim : MonoBehaviour
 
     }
 
+
+    void onAnimEnd(AnimationClip animClip)
+    {
+        ANIM animState = ANIM.NONE;
+
+        foreach (ANIM st in System.Enum.GetValues(typeof(ANIM)))
+        {
+            if (animClip.name == st.ToString())
+            {
+                animState = st;
+                break;
+            }
+        }
+
+        if (gameIsOver() && animState != ANIM.DEATH)
+        {
+            //initDeath();
+            //onAnimationEnded(animState);
+            return;
+        }
+
+        switch (animState)
+        {
+            case ANIM.DEATH:
+                return;
+
+            case ANIM.JUMP:
+                switchAnimState(PLAYER_STATE.LAND_G);
+                //controls feel better this way
+                canChange = true;
+                if (!canSprint && sprintCoolDownCoroutine == null)
+                {
+                    sprintCoolDownCoroutine = StartCoroutine(sprintCoolDown());
+                }
+                break;
+
+
+            case PLAYER_STATE.LAND_G:
+                canChange = true;
+                switchAnimState(PLAYER_STATE.RUN);
+                break;
+
+
+            case PLAYER_STATE.DODGE_R:
+            case PLAYER_STATE.ROLL:
+                switchAnimState(PLAYER_STATE.RUN, 7);
+                canChange = true;
+                if (!canSprint && sprintCoolDownCoroutine == null)
+                {
+                    sprintCoolDownCoroutine = StartCoroutine(sprintCoolDown());
+                }
+                break;
+
+
+            default:
+                switchAnimState(PLAYER_STATE.RUN);
+                canChange = true;
+                if (!canSprint && sprintCoolDownCoroutine == null)
+                {
+                    sprintCoolDownCoroutine = StartCoroutine(sprintCoolDown());
+                }
+                break;
+        }
+
+
+        onAnimationEnded(animState);
+
+    }
+
+
+    void switchAnimState(ANIM an)
+    {
+
+    }
 
 
     int getCurrFrame(AnimationClip animClip, Animator animator)
