4e977f5c2517856d0df16c5924b045b9af7ec860
Alejandro Hervella <>
2021-03-01 12:16:48 -0500

    - setup boiler plate code for doors and interactives - need to think about how to handle inputs - need to think about how to handle level states

---
+++ Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private DoorArgument doorArgument;
+    private BoxCollider2D closedColl;
+    //TODO: custom closedCollider, one way doors...
+    //TODO: inherit from Interactive class
+
+    private void Awake()
+    {
+        closedColl = GetComponentInChildren<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ASTRO"))
+        {
+            if (doorArgument == null || doorArgument.Argument())
+            {
+                //@Sean: play door open sound here
+                closedColl.enabled = false;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ASTRO"))
+        {
+            //@Sean: play door closed sound here
+            closedColl.enabled = true;
+        }
+    }
+
+    public abstract class DoorArgument : ScriptableObject
+    {
+        public abstract bool Argument();
+    }
+}
---
+++ Interactive.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Collider2D))]
+public class Interactive : MonoBehaviour
+{
+    private const string ASTRO_TAG = "ASTRO";
+    private bool ASTRO_INSIDE = false;
+    //protected Collider2D interactiveTrigger;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(ASTRO_TAG))
+        {
+            ASTRO_INSIDE = true;
+            //TODO: display text
+            //TODO: turn on update
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(ASTRO_TAG))
+        {
+            ASTRO_INSIDE = false;
+            //TODO: turn off update
+        }
+    }
+
+    private void Update()
+    {
+        //TODO: Make system to handle updates in Astro, or move to seperate script to access globaly via singleton?
+        if (!ASTRO_INSIDE)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            //@Sean: play interactive sound
+            //TODO: activate interactive
+        }
+
+    }
+}
