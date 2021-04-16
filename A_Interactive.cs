68b28a02cdfd4580853bf62f16c7dbdb412d0468
Sean Porio <SeanMPorio@gmail.com>
2021-04-16 13:13:23 -0400

    Fixed 3D functionality on A_Interactive and implemented functionality of EnvInteractive in the mindswap room to turn the sounds off when the player leaves the room.

--- A_Interactive.cs
+++ A_Interactive.cs
@@ -10,15 +10,11 @@ public abstract class A_Interactive : MonoBehaviour
     private bool astroInArea = false;
 
     //USED FOR WWISE COLLIDERS WHICH ARE 3D
-    private void OnTriggerEnter(Collider collision) 
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag(ASTRO_TAG))
         {
-            //This order is important for the interactive system work properly
-            //if we are queueing texts in OnAstroEnter
-            S_AstroInteractiveQueue.Current.AddInteractive(this);
-            OnAstroEnter(collision.gameObject);
-            astroInArea = true;
+            TriggerEnterSuccess(collision.gameObject);
         }
     }
 
@@ -27,42 +23,46 @@ public abstract class A_Interactive : MonoBehaviour
     {
         if (collision.CompareTag(ASTRO_TAG))
         {
-            //This order is important for the interactive system work properly
-            //if we are queueing texts in OnAstroEnter
-            S_AstroInteractiveQueue.Current.AddInteractive(this);
-            OnAstroEnter(collision.gameObject);
-            astroInArea = true;
+            TriggerEnterSuccess(collision.gameObject);
         }
     }
 
-    //USED FOR GENERAL PURPOSES IN 2D
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag(ASTRO_TAG))
-        {
-            OnAstroExit(collision.gameObject);
-            S_AstroInteractiveQueue.Current.RemoveInteractive(this);
-            astroInArea = false;
-
-            //Trigger on release if we left area while holding down key
-            //TODO: will this possibly trigger two OnRealses
-            if (Input.GetKey(INTERACT_KEY) || Input.GetKeyDown(INTERACT_KEY))
-            {
-                OnReleaseInteract();
-            }
-        }
-    }
-
-    //USED FOR WWISE COLLIDERS WHICH ARE 3D
     private void OnTriggerExit(Collider collision)
     {
         if (collision.CompareTag(ASTRO_TAG))
         {
-            //This order is important for the interactive system work properly
-            //if we are queueing texts in OnAstroEnter
-            S_AstroInteractiveQueue.Current.AddInteractive(this);
-            OnAstroEnter(collision.gameObject);
-            astroInArea = true;
+            TriggerExitSuccess(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(ASTRO_TAG))
+        {
+            TriggerExitSuccess(collision.gameObject);
+        }
+    }
+
+    private void TriggerEnterSuccess(GameObject astroGO)
+    {
+        //This order is important for the interactive system work properly
+        //if we are queueing texts in OnAstroEnter
+        S_AstroInteractiveQueue.Current.AddInteractive(this);
+        OnAstroEnter(astroGO);
+        astroInArea = true;
+    }
+
+    private void TriggerExitSuccess(GameObject astroGO)
+    {
+        OnAstroExit(astroGO);
+        S_AstroInteractiveQueue.Current.RemoveInteractive(this);
+        astroInArea = false;
+
+        //Trigger on release if we left area while holding down key
+        //TODO: will this possibly trigger two OnRealses
+        if (Input.GetKey(INTERACT_KEY) || Input.GetKeyDown(INTERACT_KEY))
+        {
+            OnReleaseInteract();
         }
     }
 
